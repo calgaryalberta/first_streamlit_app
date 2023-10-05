a70811859cc8972096fe703e1f5cf321a5303064
calgaryalberta <121888003+calgaryalberta@users.noreply.github.com>
2023-10-05 15:36:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruity/watermelon")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
- 
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruity/" + fruit_choice)
  
 
 
